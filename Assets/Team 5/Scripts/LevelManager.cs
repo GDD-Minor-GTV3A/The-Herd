31060a8b6d50dfe6dcb741ec0529afb775e5cc7e
jariolyslager <jariolyslager@gmail.com>
2025-10-07 14:54:30 +0200

    Added LevelManager to new scene

--- Assets/Team 5/Scripts/LevelManager.cs
+++ Assets/Team 5/Scripts/LevelManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPrefab, sheepPrefab, enemy1Prefab, enemy2Prefab;
+    [SerializeField] private GameObject playerPrefab, dogPrefab, sheepPrefab, enemy1Prefab, enemy2Prefab;
     [SerializeField] private Transform playerParent, sheepParent, enemiesParent;
     [SerializeField] private int sheepCount; // Might become a field in the Player class, in which case it wouldn't be needed here.
 
@@ -12,12 +12,13 @@ public class LevelManager : MonoBehaviour
     /// <summary>
     /// Initialization method for the dynamic parts of the level.
     /// </summary>
-    void Start()
+    void Initialize()
     {
         /* Places the playerprefab at position 0, 0, 0. Position will require changes later.
         *  Also, it currently uses the playerprefab, but the player will probably have a bunch of data attached to it, so it would need to have that data as well.
         */
-        Instantiate(playerPrefab, new Vector3(21.6790237f, 0.261999995f, 20.3814964f), Quaternion.identity, playerParent);
+        Instantiate(playerPrefab, new Vector3(71.9899979f, 24.6000004f, 275.230011f), Quaternion.identity, playerParent);
+        Instantiate(dogPrefab, new Vector3(71.9899979f, 24.6000004f, 275.230011f), Quaternion.identity, playerParent);
 
         // Places all the sheep at position 0, 0, 0. Positions will require changes later.
         for (int i = 0; i < sheepCount; i++)
