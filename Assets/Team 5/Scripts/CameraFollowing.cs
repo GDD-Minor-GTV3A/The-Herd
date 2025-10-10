cbc129a571f7a27e0699b6b5abff71082e014d2b
jariolyslager <jariolyslager@gmail.com>
2025-10-10 14:35:07 +0200

    Changed camera angle and offset for better view of the level

--- Assets/Team 5/Scripts/CameraFollowing.cs
+++ Assets/Team 5/Scripts/CameraFollowing.cs
@@ -6,12 +6,6 @@ public class CameraFollowing : MonoBehaviour
 
     void Update()
     {
-        transform.position = target.GetChild(0).position + new Vector3(-20.3f, 10.3f, -10.8f);
+        transform.position = target.GetChild(0).position + new Vector3(-25f, 30f, -25f);
     }
 }
-
-// player
-// Vector3(71.9899979,24.6000004,275.230011)
-
-// cam
-// Vector3(53.6199989,34.9599991,264.420013)
