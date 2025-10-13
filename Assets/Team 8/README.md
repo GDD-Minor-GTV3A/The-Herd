678bdbcfe1528e9f318cee02d81ca27c72f8df64
Elwqnn <elwann.guillemot@epitech.eu>
2025-10-13 11:44:06 +0200

    feat: Shaman State Machine, removing audio manager

---
+++ Assets/Team 8/README.md
@@ -0,0 +1,10 @@
+# Sprint 2
+
+## What have been done ?
+
+- [x]  State machine for the shaman
+- [ ]  Implementing rigged model
+- [ ]  Adding bells sounds
+
+## 1. State Machine
+We 
--- Assets/Team 8/Scripts/AI/Shaman.cs
+++ Assets/Team 8/Scripts/AI/Shaman.cs
@@ -1,64 +1,70 @@
 using Core.Economy;
 
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Shaman : MonoBehaviour
+namespace AI.Shaman
 {
-    [Header("References")]
-    [SerializeField] private PlayerWallet _player;
-
-    [Header("Shaman Currencies")]
-    [SerializeField] private CurrencyData _currency;
-
-    private Wallet _wallet;
-
-    void Start()
+    [RequireComponent(typeof(ShamanStateManager), typeof(NavMeshAgent))]
+    public class Shaman : MonoBehaviour
     {
-        _wallet = new Wallet();
-    }
+        [Header("References")]
+        [SerializeField] private PlayerWallet _player;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
+        [Header("Shaman Currencies")]
+        [SerializeField] private CurrencyData _currency;
+
+
+        private Wallet _wallet;
+        private ShamanStateManager _stateManager;
+
+        // for test, needs to be moved to bootstrap
+        void Start()
         {
-            Trade();
+            Initialize();
         }
-    }
 
-    public void Trade()
-    {
-        const int amount = 25;
-
-        bool success = Trading.Transfer(
-            _player.GetWallet(),
-            _wallet,
-            _currency,
-            amount
-        );
-
-        if (success)
+        /// <summary>
+        /// initialization method.
+        /// </summary>
+        public void Initialize()
         {
-            Debug.Log($"Trade successful ! {amount} {_currency.DisplayName}");
-            Debug.Log($"Shaman now has: {_wallet.GetAmount(_currency)} {_currency.DisplayName}");
-            Debug.Log($"Player now has: {_player.GetWallet().GetAmount(_currency)} {_currency.DisplayName}");
+            _wallet = new Wallet();
+            _stateManager = GetComponent<ShamanStateManager>();
+            _stateManager.Initialize(this);
         }
-        else
+
+        private void Update()
         {
-            Debug.Log("Trade failed - not enough " + _currency.DisplayName);
-            Debug.Log($"Shaman now has: {_wallet.GetAmount(_currency)} {_currency.DisplayName}");
-            Debug.Log($"Player now has: {_player.GetWallet().GetAmount(_currency)} {_currency.DisplayName}");
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Trade();
+            }
         }
-    }
 
-    private void OnGUI()
-    {
-        GUI.Label(new Rect(10, 10, 300, 20), "T=Trade | G=Give Currency");
-
-        if (_player != null && _currency != null)
+        public void Trade()
         {
-            int playerCurrency = _player.GetWallet().GetAmount(_currency);
-            int shopCurrency = _wallet.GetAmount(_currency);
-            GUI.Label(new Rect(10, 30, 300, 20), $"Player: {playerCurrency} currency | Shop: {shopCurrency} currency");
+            const int amount = 25;
+
+            bool success = Trading.Transfer(
+                _player.GetWallet(),
+                _wallet,
+                _currency,
+                amount
+            );
+
+            if (success)
+            {
+                Debug.Log($"Trade successful ! {amount} {_currency.DisplayName}");
+                Debug.Log($"Shaman now has: {_wallet.GetAmount(_currency)} {_currency.DisplayName}");
+                Debug.Log($"Player now has: {_player.GetWallet().GetAmount(_currency)} {_currency.DisplayName}");
+            }
+            else
+            {
+                Debug.Log("Trade failed - not enough " + _currency.DisplayName);
+                Debug.Log($"Shaman now has: {_wallet.GetAmount(_currency)} {_currency.DisplayName}");
+                Debug.Log($"Player now has: {_player.GetWallet().GetAmount(_currency)} {_currency.DisplayName}");
+            }
         }
     }
 }
