c05527e49ff394f69f7561765885973d67dbdaf5
Samuele Catania <samuele.catania@hotmail.com>
2025-11-17 13:45:05 +0100

    InputSystem

--- Assets/Team 11/Scripts/Core/InputSystem/InputHandler.cs
+++ Assets/Team 11/Scripts/Core/InputSystem/InputHandler.cs
@@ -12,6 +12,18 @@ namespace Core.InputSystem
         #region Events
         public UnityAction<Vector2> MovementEvent;
         public UnityAction<Vector2> LookEvent;
+        public UnityAction MainUsageEvent;
+        public UnityAction MainUsageCanceledEvent;
+        public UnityAction SecondaryUsageEvent;
+        public UnityAction SecondaryUsageCanceledEvent;
+        public UnityAction InteractEvent;
+        public UnityAction<bool> SprintEvent;
+        public UnityAction ReloadEvent;
+        public UnityAction<int> SlotsEvent;
+        public UnityAction<Vector2> SlotsScrollEvent;
+        public UnityAction BarkEvent;
+
+        public UnityAction<Vector2> NavigateEvent;
         #endregion Events
 
         void OnEnable()
@@ -20,52 +32,112 @@ namespace Core.InputSystem
             {
                 inputs = new GameInputs();
 
-                inputs.Player.AddCallbacks(this);
-                inputs.UI.AddCallbacks(this);
-            }
+                inputs.Player.SetCallbacks(this);
+                inputs.UI.SetCallbacks(this);
 
-            EnablePlayerInputs();
+                EnablePlayerInputs();
+            }
         }
+
+        void OnDisable()
+        {
+            DisableAllInputs();
+        }
+
+        #region Player Inputs
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MovementEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnMainUsage(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                MainUsageEvent?.Invoke();
+            else if (context.canceled)
+                MainUsageCanceledEvent?.Invoke();
+        }
+
+        public void OnSecondaryUsage(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                SecondaryUsageEvent?.Invoke();
+            else if (context.canceled)
+                SecondaryUsageCanceledEvent?.Invoke();
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                InteractEvent?.Invoke();
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            SprintEvent?.Invoke(context.ReadValueAsButton());
+        }
+
+        public void OnReload(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                ReloadEvent?.Invoke();
+        }
+
+        public void OnSlot_1(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                SlotsEvent?.Invoke(0);
+        }
+
+        public void OnSlot_2(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                SlotsEvent?.Invoke(1);
+        }
+
+        public void OnSlot_3(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                SlotsEvent?.Invoke(2);
+        }
+
+        public void OnSlotsScroll(InputAction.CallbackContext context)
+        {
+            SlotsScrollEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnDogBark(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                BarkEvent?.Invoke();
+        }
+        #endregion Player Inputs
+
+        #region UI Inputs
+        public void OnNavigate(InputAction.CallbackContext context)
+        {
+            NavigateEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnSubmit(InputAction.CallbackContext context) { }
 
         public void OnCancel(InputAction.CallbackContext context) { }
 
-        public void OnClick(InputAction.CallbackContext context) { }
-
-        public void OnDogBark(InputAction.CallbackContext context) { }
-
-        public void OnInteract(InputAction.CallbackContext context) { }
-
-        public void OnLook(InputAction.CallbackContext context) { }
-
-        public void OnMainUsage(InputAction.CallbackContext context) { }
-
-        public void OnMiddleClick(InputAction.CallbackContext context) { }
-
-        public void OnMove(InputAction.CallbackContext context) { }
-
-        public void OnNavigate(InputAction.CallbackContext context) { }
-
         public void OnPoint(InputAction.CallbackContext context) { }
 
-        public void OnReload(InputAction.CallbackContext context) { }
+        public void OnClick(InputAction.CallbackContext context) { }
 
         public void OnRightClick(InputAction.CallbackContext context) { }
 
+        public void OnMiddleClick(InputAction.CallbackContext context) { }
+
         public void OnScrollWheel(InputAction.CallbackContext context) { }
-
-        public void OnSecondaryUsage(InputAction.CallbackContext context) { }
-
-        public void OnSlotsScroll(InputAction.CallbackContext context) { }
-
-        public void OnSlot_1(InputAction.CallbackContext context) { }
-
-        public void OnSlot_2(InputAction.CallbackContext context) { }
-
-        public void OnSlot_3(InputAction.CallbackContext context) { }
-
-        public void OnSprint(InputAction.CallbackContext context) { }
-
-        public void OnSubmit(InputAction.CallbackContext context) { }
+        #endregion UI Inputs
 
         #region Utilities
         public void DisableAllInputs()
