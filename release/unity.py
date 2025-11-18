c079d95535190b59038bee97d0f108021fd9ff78
HEROgold <martijnwieringa28@gmail.com>
2025-11-18 23:37:38 +0100

    add log for building unity project.

--- release/unity.py
+++ release/unity.py
@@ -1,8 +1,10 @@
 """Build the project using Unity."""
 from release.args import UnityArgs
 from release.cli import run_command
+from release.log import logger
 
 
 async def build_project(args: UnityArgs) -> None:
     """Build the project using Unity.exe with the given build profile."""
+    logger.info("Building project with Unity. This may take a while...")
     await run_command(args.build_command())
