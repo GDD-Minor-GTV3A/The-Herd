21d15040974122849e37fe79ef86643fe502146c
HEROgold <martijnwieringa28@gmail.com>
2025-11-19 09:49:23 +0100

    add operator comparison support

--- release/version.py
+++ release/version.py
@@ -24,3 +24,29 @@ class SemVer:
     def bump_minor(self) -> SemVer:
         """Bump the minor version."""
         return SemVer(self.major, self.minor + 1, 0)
+
+    def __eq__(self, other: object) -> bool:
+        """Check equality."""
+        if not isinstance(other, SemVer):
+            return NotImplemented
+        return (self.major, self.minor, self.patch) == (other.major, other.minor, other.patch)
+
+    def __lt__(self, other: SemVer) -> bool:
+        """Less than comparison."""
+        return (self.major, self.minor, self.patch) < (other.major, other.minor, other.patch)
+
+    def __le__(self, other: SemVer) -> bool:
+        """Less than or equal comparison."""
+        return (self.major, self.minor, self.patch) <= (other.major, other.minor, other.patch)
+
+    def __ne__(self, other: object) -> bool:
+        """Check inequality."""
+        return not self.__eq__(other)
+
+    def __gt__(self, other: SemVer) -> bool:
+        """Greater than comparison."""
+        return not self.__le__(other)
+
+    def __ge__(self, other: SemVer) -> bool:
+        """Greater than or equal comparison."""
+        return not self.__lt__(other)
