0a037786280dc1b154d4a1c24979ec5aab0974b3
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2025-11-24 15:33:42 +0100

    Level manager door logic forest quest, add comments

--- Assets/Team 4/Scripts/Level2Manager.cs
+++ Assets/Team 4/Scripts/Level2Manager.cs
@@ -4,9 +4,13 @@ public class LevelManagerLevel2 : MonoBehaviour
 {
     public static LevelManagerLevel2 Instance { get; private set; }
     private bool ForestQuestComplete = false;
+    private bool FrontDoorReturn = false;
 
     [SerializeField] private SlidingDoor EntranceDoor, MazeDoor, ForestDoor;
 
+    /// <summary>
+    /// Create instance of level manager
+    /// </summary>
     private void Awake()
     {
         // Ensure there is only one instance
@@ -20,19 +24,44 @@ public class LevelManagerLevel2 : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Execute when entering the level
+    /// </summary>
     public void EnterLevel()
     {
         Debug.Log("Level 2 entered");
     }
 
+    /// <summary>
+    /// Execute when leaving the level
+    /// </summary>
+    public void LeaveLevel()
+    {
+        Debug.Log("Level 2 left");
+        MazeDoor.Close();
+        ForestDoor.Close();
+        FrontDoorReturn = false;
+    }
+
+    /// <summary>
+    /// Close entrance and open doors based on quest completion
+    /// </summary>
     public void EnteredCastle()
     {
-        if (EntranceDoor.isOpen)
+        // Player enters castle and front door should not remain open for return
+        if (EntranceDoor.isOpen && !FrontDoorReturn)
         {
             EntranceDoor.Lock();
             EntranceDoor.Close();
         }
 
+        // Front door should be open for return, but is closed
+        if (FrontDoorReturn && !EntranceDoor.isOpen)
+        {
+            EntranceDoor.Open();
+        }
+
+        // Choose which door in the castle to open
         if (ForestQuestComplete)
         {
             MazeDoor.Open();
@@ -41,6 +70,16 @@ public class LevelManagerLevel2 : MonoBehaviour
         {
             ForestDoor.Open();
         }
+    }
+
+    /// <summary>
+    /// Execute when player finished the forest quest, make entrance door open for return
+    /// </summary>
+    public void FinishedForestQuest()
+    {
+        ForestQuestComplete = true;
+        FrontDoorReturn = true;
+        EntranceDoor.Open();
     }
     
 }
