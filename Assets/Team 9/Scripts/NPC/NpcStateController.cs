c7b741d31b7719a5d28f90d4bb671470e349a880
Vincent <167082919+Pr1sm5@users.noreply.github.com>
2025-12-08 15:33:38 +0100

    Add Vesna3DCharacter model and update NPC scripts
    Added Vesna3DCharacter.fbx and its meta file to Team 9 models. Renamed NPCState.cs to NpcStateController.cs and updated corresponding meta file. Updated project and material files, and modified csproj references to use new Unity Hub installation paths.

--- Assets/Team 9/Scripts/NPC/NpcStateController.cs
+++ Assets/Team 9/Scripts/NPC/NpcStateController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
-public class NPCState : MonoBehaviour
+public enum NpcState
+{
+    IDLE,
+    ROAMING,
+    TALKING,
+    NOTHING
+}
+
+
+public class NpcStateController : MonoBehaviour
 {
     //TODO: IMPLEMENT NPC STATE SYSTEM FOR ANIMATIONS/SOUNDS/BREAK LOGIC (IF TALKED TO WHILE WALKING)
     // Start is called once before the first execution of Update after the MonoBehaviour is created
