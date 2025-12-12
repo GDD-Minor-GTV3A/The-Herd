de4ea010c3f3d7a08dbf9ca7c1862f417330b33d
WolfRedPlay <126975145+WolfRedPlay@users.noreply.github.com>
2025-12-12 16:28:40 +0100

    Fixed prefab

--- Assets/Team 1/Scripts/Gameplay/SanityEffects/EffectsTest.cs
+++ Assets/Team 1/Scripts/Gameplay/SanityEffects/EffectsTest.cs
@@ -1,13 +1,19 @@
+using Gameplay.CameraSettings;
 using Gameplay.Player;
 using UnityEngine;
 
 public class EffectsTest : MonoBehaviour
 {
+    public CameraManager CameraManager;
+
+    private bool isActive = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            PlayerInputHandler.SwitchControlMap();
+            isActive = !isActive;
+            CameraManager.SetContinuousCameraShakes(isActive, 2.0f);
         }
     }
 }
