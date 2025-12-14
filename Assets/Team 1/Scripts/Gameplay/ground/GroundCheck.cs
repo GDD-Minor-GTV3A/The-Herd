ddfa0c623895bd6d4d61b49fa4e98c11d732d7bc
Nikootine <58007941+Nikootine@users.noreply.github.com>
2025-12-14 19:39:26 +0100

    GroundAudio

--- Assets/Team 1/Scripts/Gameplay/ground/GroundCheck.cs
+++ Assets/Team 1/Scripts/Gameplay/ground/GroundCheck.cs
@@ -10,13 +10,18 @@ public class GroundCheck : MonoBehaviour
     [SerializeField]
     private List<GroundType> groundTypes = new List<GroundType>();
 
+    [SerializeField]
+    private GroundType currentGroundType;
+
+    public GroundType CurrentGroundType => currentGroundType;
+
     void Update()
     {
         int textureIndex = GetMainTexture(transform.position);
 
         GroundType _currentGroundType = groundTypes.Find(x => x.TextureIndex == textureIndex);
 
-        Debug.Log($"Standing on {_currentGroundType.GroundSurface}");
+        //Debug.Log($"Standing on {_currentGroundType.GroundSurface}");
     }
 
     int GetMainTexture(Vector3 worldPos)
@@ -45,23 +50,38 @@ public class GroundCheck : MonoBehaviour
 
         return dominant;
     }
-}
+    public GroundSurface GetGroundType()
+    {
+        int textureIndex = GetMainTexture(transform.position);
+
+        foreach (var groundType in groundTypes)
+        {
+            if (groundType.TextureIndex == textureIndex)
+            {
+                currentGroundType = groundType;
+                return groundType.GroundSurface;
+            }
+        }
+        Debug.LogWarning($"No GroundType found for texture index {textureIndex}");
+        return GroundSurface.Dirt;
+    }
 
 
-[Serializable]
-public struct GroundType
-{
-    [field: SerializeField]
-    public int TextureIndex { get; private set; }
+    [Serializable]
+    public struct GroundType
+    {
+        [field: SerializeField]
+        public int TextureIndex { get; private set; }
 
-    [field: SerializeField]
-    public GroundSurface GroundSurface { get; private set; }
-}
+        [field: SerializeField]
+        public GroundSurface GroundSurface { get; private set; }
+    }
 
-public enum GroundSurface
-{
-    Dirt,
-    Snow,
-    Rock,
-    Ice
+    public enum GroundSurface
+    {
+        Dirt,
+        Snow,
+        Rock,
+        Ice
+    }
 }
